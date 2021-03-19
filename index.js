efbfa376f6b7105a0a3b27a3e395d7a62bbad882
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-03-19 12:40:15 -0600

    Add delete API call

--- index.js
+++ index.js
@@ -39,6 +39,9 @@ function validateGenre(genre) {
   return schema.validate(genre)
 }
 
+app.get('/api/genres', (req, res) => {
+  res.send(genres);
+});
 
 app.post('/api/genres', (req, res) => {
   const genre = {
@@ -49,26 +52,39 @@ app.post('/api/genres', (req, res) => {
   res.send(genre);
 });
 
-app.get('/api/genres', (req, res) => {
-  res.send(genres);
-});
 
 app.get('/api/genres/:id', (req, res) => {
-  let genre = genres.find((g) => g.id === parseInt(req.params.id))
-  if(!genre) {
-    return res.status(404).send('The genre with the given ID was not found');
+  const genre = genres.find((g) => g.id === parseInt(req.params.id))
+  if (!genre) {
+    return res.status(404).send('There is no genre with the given ID')
   }
-})
+  res.send(genre)
+} )
 
 app.put('/api/genres/:id', (req, res) => {
-  let genre = genres.find((g) => g.id === parseInt(req.params.id));
+  let genre = genres.find((g) => g.id === parseInt(req.params.id))
+  if(!genre) {
+    return res.status(404).send('There is no genre found with the given ID')
+  }
+  const { error }= validateGenre(req.body)
+  if(error) {
+    return res.status(400).send(error.details[0].message)
+  }
+  genre.name = req.body.name
+  res.send(genre)
+})
+ 
+app.delete('/api/genres/:id', (req, res) => {
+    let genre = genres.find((g) => g.id === parseInt(req.params.id));
     if (!genre) {
-      return res.status(404).send('The genre with the given ID was not found');
+      return res.status(404).send('There is no genre found with the given ID');
     }
-    const { error } = validateGenre(req.body)
-    if(error) {
-      return res.status(400).send(error.details[0].message);
-    }
-    genre.name = req.body.name
+    const index = genres.indexOf(genre)
+    genres.splice(index, 1)
     res.send(genre)
 })
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`))
