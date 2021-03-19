5abb8ca83118cb29c0262145c1009e8903ec52bc
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-03-19 09:48:37 -0600

    Add GET API call for genre ID

--- index.js
+++ index.js
@@ -42,3 +42,9 @@ app.get('/api/genres', (req, res) => {
   res.send(genres);
 });
 
+app.get('/api/genres/:id', (req, res) => {
+  let genre = genres.find((g) => g.id === parseInt(req.params.id))
+  if(!genre) {
+    return res.status(404).send('The genre with the given ID was not found');
+  }
+})
