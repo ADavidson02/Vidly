96537620903b48a67265cd19aba41866c470b130
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-03-19 09:47:21 -0600

    Add movie genres

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const app = express();
+
+const genres = [
+  {
+    id: 1,
+    name: 'action',
+  },
+  {
+    id: 2,
+    name: 'horror',
+  },
+  {
+    id: 3,
+    name: 'romance',
+  },
+  {
+    id: 4,
+    name: 'drama',
+  },
+  {
+    id: 5,
+    name: 'comedy',
+  },
+  {
+    id: 6,
+    name: 'documentary',
+  },
+];
+
+
